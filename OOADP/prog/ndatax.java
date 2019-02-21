234dfbebc4fd4591a80a91bb93d480d4f43bddc0
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-02-21 12:29:12 +0530

    Create ndatax.java

---
+++ OOADP/prog/ndatax.java
@@ -0,0 +1,11 @@
+package ooadplab;
+
+
+public class ndatax implements CaclTax {
+	nda n1 = new nda();
+public double taxAmount(double price,int quantity)
+	{
+		return nda.newtax(price)*quantity;
+	}
+
+}
