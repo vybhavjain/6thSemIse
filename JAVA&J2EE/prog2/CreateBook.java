2fdf9b6d9d052baa5e91e986ad4f0ed1c9225382
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-03-01 12:42:07 +0530

    Update CreateBook.java

--- JAVA&J2EE/prog2/CreateBook.java
+++ JAVA&J2EE/prog2/CreateBook.java
@@ -1,8 +1,6 @@
 public class CreateBook {
 	String title,author,publisher;
 	float price;
-	
-
 	CreateBook(String t,String a,String p,float pr){
 
 		this.title=t;
@@ -10,10 +8,7 @@ public class CreateBook {
 		this.publisher=p;
 		this.price=pr;
 	}
-	
 	public String toString() {
 		  return "\n\n title=" + title + "\n auther= " + author + "\n publisher = " + publisher + "\n price = " +price;
 		}
-
-
 }
