490536fff459af7d53cb5f32a90cfa7082ea2468
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-04-01 15:40:27 +0530

    Update part3.c

--- SystemSoftware/absolute/part3.c
+++ SystemSoftware/absolute/part3.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 #include<string.h>
 char input[10],label[10],ch1,ch2;
 int addr, w=0, start, ptaddr, l, length=0, end, count=0, k, taddr, address, i=0;
@@ -8,9 +7,9 @@ void check();
 void main()
 {  
   fp1=fopen("try1.txt","r");
-  fp2=fopen("OUTPUT.dat","w");
+  fp2=fopen("output.txt","w");
   fscanf(fp1,"%s",input);
-  printf("\n\n\t\t\t\tABSOLUTE LOADER\n");
+  printf("Output generated in output.txt file \n");
   fprintf(fp2,"\n-------------------------------------------------------\n");
   fprintf(fp2,"MEMORY ADDRESS\t\t\tCONTENTS");
   fprintf(fp2,"\n-------------------------------------------------------\n");
@@ -71,16 +70,9 @@ void main()
    }
   }
   fprintf(fp2,"\n-------------------------------------------------------\n");
-  fcloseall();
-  printf("\n\n The contents of output file:\n\n");
-  fp2=fopen("OUTPUT.DAT","r");
-  ch2=fgetc(fp2);
-  while(ch2!=EOF)
-  {
-   printf("%c",ch2);
-   ch2=fgetc(fp2);
-  }
-  fcloseall();
+  //fcloseall();
+  fclose(fp1);
+  fclose(fp2);
 }
 void check()
 {
