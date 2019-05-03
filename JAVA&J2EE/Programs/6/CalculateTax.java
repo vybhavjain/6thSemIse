41390b1e019b4a2d4a6547aeed4900024dfa83fc
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-03 10:03:48 +0530

    Update CalculateTax.java

--- JAVA&J2EE/Programs/6/CalculateTax.java
+++ JAVA&J2EE/Programs/6/CalculateTax.java
@@ -1,22 +1,16 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/CalculateTax")
 public class CalculateTax extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     public CalculateTax() {
-        // TODO Auto-generated constructor stub
     }
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -45,10 +39,8 @@ public class CalculateTax extends HttpServlet {
 
 		pw.close();
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
