dd93b01cb6ad81b4f1816e0af861227252935c45
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-29 18:40:37 +0530

    Update 5.java

--- JAVA&J2EE/Programs/5.java
+++ JAVA&J2EE/Programs/5.java
@@ -1,115 +1,68 @@
-package trial;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import java.io.*;
+import java.nio.file.*;
+import javax.swing.*;
 
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Scanner;
+public class ProgressBar {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setLayout(new FlowLayout());
+        f.setSize(800, 800);
+        JLabel l1 = new JLabel("Source");
+        JLabel l2 = new JLabel("Destination");
+        JTextField t1 = new JTextField(30);
+        JTextField t2 = new JTextField(30);
+        JButton b1 = new JButton("Start");
+        JButton b2 = new JButton("Stop");
+        JTextArea ta = new JTextArea(40, 40);
+        f.add(l1);
+        f.add(t1);
+        f.add(l2);
+        f.add(t2);
+        f.add(b1);
+        f.add(ta);
+        f.setVisible(true);
+        JProgressBar pb = new JProgressBar(0, 100);
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JTextField;
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String src = t1.getText();
+                String dest = t2.getText();
+                pb.setIndeterminate(true);
+                pb.setString("Copying");
+                pb.setStringPainted(true);
+                pb.setVisible(true);
+                try {
 
-public class progressbar extends JFrame implements Runnable,ActionListener {
+                    f.add(pb);
+                    f.add(b2);
+                    f.setVisible(true);
+                    String content = new String(Files.readAllBytes(Paths.get(src)));
+                    PrintWriter outPrintWriter = new PrintWriter(dest);
+                    outPrintWriter.println(content);
+                    outPrintWriter.close();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
 
-	JPanel jp;
-	JProgressBar jpb;
-	JLabel fromLabel,toLabel;
-	JTextField fromName,toName;
-	JButton copyButton,stopButton;
-	BufferedInputStream inBuffer;
-	BufferedOutputStream outBuffer;
-	public progressbar() {
-		setLayout(new GridLayout(0,1));
-		
-		jp=new JPanel();
-		add(jp);
-		jpb=new JProgressBar(0, 100);
-		jpb.setStringPainted(true);
-		jp.add(jpb);
-		
-		fromLabel=new JLabel("Source File:");
-		fromName =new JTextField();
-		toLabel=new JLabel("Destination File:");
-		toName =new JTextField();
-		
-		copyButton=new JButton("START");
-		stopButton=new JButton("STOP");
-		
-		copyButton.addActionListener(this);
-		stopButton.addActionListener(this);
-		
-		add(fromLabel);add(fromName);
-		add(toLabel);add(toName);
-		add(copyButton);add(stopButton);
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==copyButton) {
-			run();
-		}
-		else if(e.getSource()==stopButton) {
-			try {
-			
-				if(inBuffer!=null)
-					inBuffer.close();
-				if(outBuffer!=null)
-					outBuffer.close();
-				inBuffer=new BufferedInputStream(new FileInputStream(toName.getText()));
-				Scanner content=new Scanner(inBuffer);
-				while(content.hasNextLine()) {
-					System.out.println(content.nextLine());
-				}
-			}catch (IOException except) {
-				except.printStackTrace();
-			}
-		}
-	}
+            }
+        });
 
-	@Override
-	public void run() {
-		String inFile=fromName.getText();
-		String outFile=toName.getText();
-		
-		File in=new File(inFile);
-		File out=new File(outFile);
-		byte input[]=new byte[10];
-		
-		int total=0,rcount=0,tcount=0;
-		try {
-			out.createNewFile();
-			inBuffer=new BufferedInputStream(new FileInputStream(in));
-			outBuffer=new BufferedOutputStream(new FileOutputStream(out));
-			total=inBuffer.available();
-			while((rcount=inBuffer.read(input))!=-1) {
-				outBuffer.write(input);
-				tcount+=rcount;
-				jpb.setValue((int)(tcount*100)/total);
-			}
-			
-			inBuffer.close();
-			outBuffer.close();
-					
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        b2.addActionListener(new ActionListener() {
 
-	}
-
-	public static void main(String[] args) {
-		
-		progressbar fp=new progressbar();
-		fp.setSize(200, 200);
-		fp.setVisible(true);
-	}
+            public void actionPerformed(ActionEvent e) {
+                String src = t2.getText();
+                pb.setVisible(false);
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                try {
+                    String content = new String(Files.readAllBytes(Paths.get(src)));
+                    ta.setText(content);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
 }
