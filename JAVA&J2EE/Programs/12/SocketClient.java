f8e9c8d3e86f14708b8d6ffdc1854fa70cceca4d
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-29 15:59:16 +0530

    Update SocketClient.java

--- JAVA&J2EE/Programs/12/SocketClient.java
+++ JAVA&J2EE/Programs/12/SocketClient.java
@@ -7,7 +7,7 @@ public class SocketClient {
 	public static void main(String[] args) {
 		try {
 			Socket s = new Socket("localhost", 1999);
-			BufferedReader reader = new BufferedReader(new FileReader("/Users/hemantj/eclipse-workspace/Socket/hello.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("hello.txt"));
 			byte []b = new byte[30]; 
 			String k = reader.readLine(); 
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
