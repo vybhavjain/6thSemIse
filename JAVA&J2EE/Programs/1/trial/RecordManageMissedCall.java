cc17199b0d6cc6791c83184939e564f0b889f18d
nimishbongale <nimishnb98@gmail.com>
2020-02-05 09:47:52 +0530

    Changes

--- JAVA&J2EE/Programs/1/trial/RecordManageMissedCall.java
+++ JAVA&J2EE/Programs/1/trial/RecordManageMissedCall.java
@@ -11,8 +11,7 @@ public class RecordManageMissedCall {
 		amiss = new LinkedList<MissedCallDetails>();
 		while(true)
 		{
-			System.out.println("Enter a choice");
-			System.out.println("1:Add missed call\n2.Display and delete on request\n3.Delete based on number\n4.Print");
+			System.out.println("Enter a choice\n1:Add missed call\n2.Display and delete on request\n3.Delete based on number\n4.Print");
 			int choice = DataBaseCallers.sc.nextInt();
 			switch(choice)
 			{
