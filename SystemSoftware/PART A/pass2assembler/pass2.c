3eee9c456f58d0a3731c92a1ba141a415aaae074
nimishbongale <nimishnb98@gmail.com>
2020-02-19 15:27:07 +0530

    Changes

--- SystemSoftware/PART A/pass2assembler/pass2.c
+++ SystemSoftware/PART A/pass2assembler/pass2.c
@@ -6,64 +6,64 @@ void main()
 	FILE *f1,*f2,*f3,*f4;
 	char label[20],opcode[20],operand[20];
 	int address,sa,i;
-	f1=fopen("output.txt","r");
-	f4=fopen("output2.txt","w");
-	fscanf(f1,"%x %s %s %s",&address,label,opcode,operand);
-	if(strcmp(opcode,"START")==0)
+	f1=fopen("output.txt","r");//output of pass1 
+	f4=fopen("output2.txt","w");//output generated from pass2 containing object codes
+	fscanf(f1,"%x %s %s %s",&address,label,opcode,operand);//read from pass1 
+	if(strcmp(opcode,"START")==0)//find start
 	{
-		fprintf(f4,"%X\t%s\t%s\t%s\t-\n",address,label,opcode,operand);
-		sa=address;
+		fprintf(f4,"%X\t%s\t%s\t%s\t-\n",address,label,opcode,operand);//- since start doesnt have objcode
+		sa=address;//start address 
 	}
-	fscanf(f1,"%x %s %s %s",&address,label,opcode,operand);
-	while(strcmp(opcode,"END")!=0)
+	fscanf(f1,"%x %s %s %s",&address,label,opcode,operand);//read next line from pass1 output
+	while(strcmp(opcode,"END")!=0)//while not the END 
 	{
-		fprintf(f4,"%X\t%s\t%s\t%s\t",address,label,opcode,operand);
-		f2=fopen("optab1.txt","r");
+		fprintf(f4,"%X\t%s\t%s\t%s\t",address,label,opcode,operand);//write the pass1 results into output
+		f2=fopen("optab1.txt","r");//open optab and read
 		char tempcode[20],tempval[20];
-		fscanf(f2,"%s %s",tempcode,tempval);
-		while(!feof(f2))
+		fscanf(f2,"%s %s",tempcode,tempval);//read from optab
+		while(!feof(f2))//while its not an end of file
 		{
-			if(strcmp(opcode,tempcode)==0)
+			if(strcmp(opcode,tempcode)==0)//if operation code is found 
 			{
-				fprintf(f4,"%s",tempval);
-				break;
+				fprintf(f4,"%s",tempval);//write the operation code
+				break;//exit 
 			}
 			else
-				fscanf(f2,"%s %s",tempcode,tempval);
+				fscanf(f2,"%s %s",tempcode,tempval);//else just read and continue
 		}
-		fclose(f2);
-		char actoperand[20];
-		strcpy(actoperand,"");
-		if(operand[strlen(operand)-1]=='X')
+		fclose(f2);//close optab1
+		char actoperand[20];//actual operand to write into output 
+		strcpy(actoperand,"");//copy blank
+		if(operand[strlen(operand)-1]=='X')//if indexed
 		{
 			int i;
 			for(i=0;i<strlen(operand)-2;i++)
-				actoperand[i]=operand[i];
-			actoperand[i]='\0';
+				actoperand[i]=operand[i];//find operand,copy into actoperand
+			actoperand[i]='\0';//terminate with null
 		}
 		else
-			strcpy(actoperand,operand);
-		f3=fopen("symtab.txt","r");
+			strcpy(actoperand,operand);//if not indexed,straight copy
+		f3=fopen("symtab.txt","r");//symbol table in read mode
 		char symcode[20],symval[20];
-		fscanf(f3,"%s %s",symcode,symval);
-		while(!feof(f3))
+		fscanf(f3,"%s %s",symcode,symval);//read from symtab
+		while(!feof(f3))//while not EOF of symtab
 		{
-			if(strcmp(actoperand,symcode)==0)
+			if(strcmp(actoperand,symcode)==0)//if actual operand is found in symtab
 			{
 				
-				int lval=strtol(symval,NULL,16);
-				if(operand[strlen(operand)-1]=='X')
-					lval=lval+0x8000;
-				fprintf(f4,"%X\n",lval);
+				int lval=strtol(symval,NULL,16);//convert to base16(hex)
+				if(operand[strlen(operand)-1]=='X')//if indexed 
+					lval=lval+0x8000;//!!!!???
+				fprintf(f4,"%X\n",lval);//write value in hex 
 				break;
 			}
 			else
-				fscanf(f3,"%s %s",symcode,symval);
+				fscanf(f3,"%s %s",symcode,symval);//read from symtab
 			
 		}
-		fclose(f3);
+		fclose(f3);//close symtab
 
-		if(strcmp(opcode,"WORD")==0)
+		if(strcmp(opcode,"WORD")==0)//if 
 		{
 			int val=strtol(operand,NULL,16);
 			char num[10];
