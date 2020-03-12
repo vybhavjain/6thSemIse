3e936036e54c6e9a07782976533ae0c7ee434284
Nimish Bongale <nimishnb98@gmail.com>
2020-03-12 07:44:22 +0530

    Changes

--- SystemSoftware/PART A/pass1assembler/pass1.c
+++ SystemSoftware/PART A/pass1assembler/pass1.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
 
 void main()
 {
@@ -28,18 +27,16 @@ void main()
 		fprintf(f4,"%X\t%s\t%s\t%s\n",lc,label,opcode,operand);//write into output file
 		
 		if(strcmp(label,"*")!=0)//if * means no label, don't write into symtab
-		{
 			fprintf(f3,"%s\t%X\n",label,lc);//write into symtab
-		}
 		
 		char tempcode[20],tempval[20];//stores contents of optab1; code, code id
-		f2=fopen("optab1.txt","r");
-		fscanf(f2,"%s %s",tempcode,tempval);
+		f2=fopen("optab1.txt","r");//f2 for obtab1 
+		fscanf(f2,"%s %s",tempcode,tempval);//read from optab1
 		while(!feof(f2))//while not end of file
 		{
 			if(strcmp(opcode,tempcode)==0)//compare the opcode and codes in optab1
 			{
-				lc+=3;//default increment 3
+				lc+=3;//default increment 3,format 3 instructions
 				break;
 			}
 			fscanf(f2,"%s %s",tempcode,tempval);//read the next instruction 
@@ -47,30 +44,24 @@ void main()
 		fclose(f2);//close optab1
 
 		if(strcmp(opcode,"WORD")==0)//if opcode is WORD
-		{
 			lc+=3;//add 3
-		}
 
 		if(strcmp(opcode,"RESW")==0)
-		{
-			lc=lc+(3*(strtol(operand,NULL,10)));//convert to decimal , multiple by 3 and add to lc
-		}
+			lc=lc+(3*(strtol(operand,NULL,10)));//convert to decimal , multiply by 3 and add to lc
 
 		if(strcmp(opcode,"RESB")==0)
-		{
 			lc=lc+strtol(operand,NULL,10);//convert to decimal and add to lc
-		}
 
 		if(strcmp(opcode,"BYTE")==0)//if opcode is byte
 		{
 			if(operand[0]=='X')//if hex 
 				lc++;
 			else
-				lc=lc+strlen(operand)-3;//if string of char
+				lc=lc+strlen(operand)-3;//if string of char (c'eof')
 		}
 		fscanf(f1,"%s %s %s",label,opcode,operand);//default read from input
 	}
-	fprintf(f4,"%X\t%s\t%s\t%s\n",lc,label,opcode,operand);//write into 
+	fprintf(f4,"%X\t%s\t%s\t%s\n",lc,label,opcode,operand);//write into output the final instruction E
 
 	printf("Output File generated as output.txt\n");//generate output 
 	fclose(f1);
